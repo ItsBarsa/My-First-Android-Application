03504876ae9df3daa33a844da6bfc2bd524c58f9
Barsha23 <barshapokhrel23@gmail.com>
2022-02-05 14:16:49 +0545

    menu and submenu is added to home page layout

--- app/src/main/java/com/barsha/first_android_application/HomeActivity.java
+++ app/src/main/java/com/barsha/first_android_application/HomeActivity.java
@@ -1,14 +1,22 @@
 package com.barsha.first_android_application;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
     Button logout;
@@ -19,6 +27,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage_layout);
+
         sharedPreferences = getSharedPreferences("userinfo", MODE_PRIVATE);
 
         logout = findViewById(R.id.logout);
@@ -38,6 +47,81 @@ public class HomeActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu1:
+                startActivity(new Intent(HomeActivity.this, RegisterActivity.class));
+                break;
+
+            case R.id.menu2:
+                showAlertDialog();
+                break;
+
+            case R.id.menu3:
+                Toast.makeText(this, "Menu3", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menu4:
+                showCustomToast("This is custom toast");
+                break;
+
+            case R.id.menu5:
+
+                break;
+
+            case R.id.submenu:
+                showCustomToast("submenu");
+                break;
+
+            case R.id.submenu1:
+                showCustomToast("submenu1");
+                break;
+
+            case R.id.submenu2:
+                showCustomToast("submenu2");
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showAlertDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Exit application");
+        dialog.setMessage("Are you sure?");
+        dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                finish();
+            }
+        });
+        dialog.setNegativeButton("cancel", null);
+        dialog.setCancelable(false);
+        dialog.show();
+
+    }
+
+    public void showCustomToast(String srt) {
+        Toast toast = new Toast(this);
+        View view = LayoutInflater.from(this).inflate(R.layout.custom_toast_design, null);
+        TextView message = view.findViewById(R.id.message);
+        message.setText(srt);
+        toast.setView(view);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.TOP, 0, 100);
+        toast.show();
     }
 
 }
